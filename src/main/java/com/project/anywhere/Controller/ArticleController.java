18e76f51560fe06616e1932698d494c38f2d1abe
현규 장 <wkdgusrb3@naver.com>
2022-10-05 18:13:50 +0900

    게시글 삭제 구현
    Method: Delete 
    URL: masil/{id}
    id에 해당되는 게시글을 삭제한다.

--- src/main/java/com/project/anywhere/Controller/ArticleController.java
+++ src/main/java/com/project/anywhere/Controller/ArticleController.java
@@ -1,27 +1,52 @@
 package com.project.anywhere.Controller;
 
 import com.project.anywhere.DTO.Article;
+import com.project.anywhere.Errors.notExists;
 import com.project.anywhere.Repository.ArticleRepository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.net.ssl.SSLEngineResult;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @RestController
-public class ArticleController {
+public class ArticleController extends ArticleRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+    @Transactional
     @PostMapping("masil")
     public String writeArticle(@RequestBody Article article){
         try {
-            ArticleRepository articleRepository = new ArticleRepository();
-            articleRepository.writeArticle(article);
+            writeArticle(article,em);
         }
         catch(Exception e){
             return "{error:402}";
         }
         return SSLEngineResult.Status.OK.toString();
     }
+    @Transactional
     @DeleteMapping("masil/{id}")
-    public SSLEngineResult.Status deleteArticle(@PathVariable(name="id") int article_id){
+    public String deleteArticle(@PathVariable(name="id") int article_id){
         System.out.println(article_id);
-        return SSLEngineResult.Status.OK;
+        try {
+            deleteArticle(article_id,em);
+        }
+        catch (notExists e){
+            System.out.println("not Exists");
+            return "NOT OKAY";
+        }
+        return "OK";
+    }
+    @Transactional
+    @GetMapping("masil/{id}")
+    public String getArticle(@PathVariable int article_id){
+        return SSLEngineResult.Status.OK.toString();
+    }
+    @Transactional
+    @GetMapping("masil/all")
+    public String getArticles(){
+        return "Test";
     }
 }
--- src/main/java/com/project/anywhere/Repository/ArticleRepository.java
+++ src/main/java/com/project/anywhere/Repository/ArticleRepository.java
@@ -3,18 +3,17 @@ package com.project.anywhere.Repository;
 import com.project.anywhere.DTO.Article;
 import com.project.anywhere.DTO.Articles;
 import com.project.anywhere.DTO.Places;
+import com.project.anywhere.Errors.notExists;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import java.util.List;
 import java.util.Optional;
 @Repository
 public class ArticleRepository {
-    @PersistenceContext
-    private EntityManager em;
-    @Transactional
-    public  String writeArticle( Article article){
+    //삽입
+    public  String writeArticle( Article article,EntityManager em){
 
         try{
             Places places=em.find(Places.class,article.getAddr());
@@ -23,17 +22,45 @@ public class ArticleRepository {
                 places=new Places();
                 places.setPlace_id(article.getAddr());
             }
+            articles = new Articles();
             articles.setAll(article, Optional.of(places));
             places.getArticles().add(articles);
+
             em.persist(articles);
             em.persist(places);
             em.flush();
         }
         catch(Exception e){
             System.out.println(e.toString());
-
+            return "bad";
         }
         em.close();
         return "good";
+    }
+    //삭제
+    public void deleteArticle(long article_id,EntityManager em) throws notExists {
+        Articles articles =null;
+        articles = em.find(Articles.class,article_id);
+        if(articles==null){
+            throw new notExists();
+        }
+        else{
+            em.remove(articles);
+            em.flush();
+        }
+        return;
+    }
+    //수정
+    public void updateArticles(Article article, long article_id, EntityManager em){
+        
+
+    }
+    //조회1
+    public List<Articles> getArticles(String Addr, EntityManager em){
+        Places places = em.find(Places.class,Addr);
+        if(places==null){
+            return null;
+        }
+        return places.getArticles();
     }
 }
